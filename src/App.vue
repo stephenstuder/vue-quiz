d839ba0f20b3efb61a3983d56d1acc6e64e6b9fb
Stephen Studer <stephenkstuder@gmail.com>
2020-08-09 17:47:29 -0700

    logic for correct

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,32 @@
     <hr />
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <component :is="mode"></component>
+        <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Answer from "./components/Answer.vue";
 import Question from "./components/Question.vue";
+import Answer from "./components/Answer.vue";
+
 export default {
   data() {
     return {
       mode: "app-question",
     };
   },
-  methods: {},
+  methods: {
+    answered(isCorrect) {
+      if (isCorrect) {
+        this.mode = "app-answer";
+      } else {
+        this.mode = "app-question";
+        alert("Wrong, try again!");
+      }
+    },
+  },
   components: {
     appQuestion: Question,
     appAnswer: Answer,
